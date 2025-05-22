79492d8e556ec1d6e3a0c10c2cd4a2b623dbb827
sashavershkova <a.m.vershkova@gmail.com>
2025-05-22 14:21:01 -0700

    items 1-3 done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,46 @@
 //    (Ex: make parrot bigger, smaller, change the text/emoji,
 //    etc.)
 
+const state = {
+  parrotCount: 0
+};
+
+
+const addParrotCount = () => {
+  state.parrotCount += 1;
+  const countContainer = document.getElementsByTagName('h1')[0];
+  countContainer.textContent = `Clicks: ${state.parrotCount}`;
+
+  addEffectOnEleven();
+};
+
+const resetClicks = () => {
+  state.parrotCount = 0;
+  const countContainer = document.getElementsByTagName('h1')[0];
+  countContainer.textContent = `Clicks: ${state.parrotCount}`;
+};
+
+const addEffectOnEleven = () => {
+  if (state.parrotCount === 11) {
+    const partyParrot = document.getElementsByTagName('img')[0];
+    partyParrot.width = 200;
+  };
+};
+
+
+const registerEventHandlers = () => {
+  const partyParrot = document.getElementsByTagName('img')[0];
+  partyParrot.addEventListener('click', addParrotCount);
+  const resetClickButton = document.getElementById('reset-button');
+  resetClickButton.addEventListener('click', resetClicks);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+
+
+
+
 // Extra:
 // // Box 1: When the mouse hovers over Box 1, switch the background to pink
 // Box 2: When the spacebar is pressed down, make the text in Box 2 turn into a flan emoji
